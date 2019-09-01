810acde9dfa1fc9faee528a082fd546618f845da
Cyprian Munene <munenecyp@gmail.com>
2019-09-01 21:09:27 +0300

    define product and category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,8 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @category = Category.find(params[:id])
+    @products = @category.products.all
   end
 
   # GET /categories/new
