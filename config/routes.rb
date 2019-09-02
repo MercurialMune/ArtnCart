ffb1d70a77ab9fd08c19b1116ecccb44d7d8d2b9
Cyprian Munene <munenecyp@gmail.com>
2019-09-03 00:55:23 +0300

    build cart controller, view and template

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  get 'carts/show'
   devise_for :users
   devise_for :admins
-  
+
   resources :categories, only: [:show]
   resources :products, only: [:index, :show]
   get 'welcome/index'
   root 'welcome#index'
+
+  resource :cart, only: [:show] do
+    put 'add/:product_id', to: 'carts#add', as: :add_to
+    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
+    put 'remove_one/:product_id', to: 'carts#remove_one', as: :remove_one
+  end
 
   namespace :admin do
     root to: 'products#index'
     resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
     resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
   end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
