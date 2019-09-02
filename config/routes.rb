3fc43b05f0f62ac892f60695ebcd2fe48201d661
Cyprian Munene <munenecyp@gmail.com>
2019-09-02 14:06:29 +0300

    add product and category controller for admin to add product and category objects to the db

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+  namespace :admin do
+    root to: 'products#index'
+    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
